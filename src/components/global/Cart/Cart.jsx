3325e14245aaf2f50ea4f4348945cce110275178
GastonLell <gastonglell@gmail.com>
2021-01-12 15:44:41 -0300

    delete item cart

--- src/components/global/Cart/Cart.jsx
+++ src/components/global/Cart/Cart.jsx
@@ -1,28 +1,37 @@
 import {CartStyle, Delete} from './CartStyle';
 import CountContainer from '../Count/CountContainer';
 
-const Cart = ({item, cantidadItem}) => {
+const Cart = ({item, cantidadItem, idVenta, removeItem}) => {
+    
     return(
         <CartStyle>
             <div className="detalle">
+
                 <img src="https://placehold.it/100x120" alt="imagen-producto"/>
+
                 <div className="info">
                     <h2>{item.nombreProducto}</h2>
                     <h3>${item.precioProducto}</h3>
                 </div>
+
             </div>
+
             <div className="count">
+
                 {/* contador: le paso la cantidad seleccionada del item
                 para poder agregarle esa cantidad en cart */}
                 <CountContainer cantidadItem={cantidadItem} stock={item.stock}/>
                 {
                     !!item.stock > 1 ? <span>1 elemento disponible</span> : <span>{item.stock} elementos disponibles</span>
                 }
+
             </div>
+
             <div className="sub-total">
                 ${item.precioProducto * cantidadItem}
             </div>
-            <Delete onClick={() => alert("hola!")}>X</Delete> 
+
+            <Delete onClick={() => removeItem(item.idProducto, idVenta)}>X</Delete> 
         </CartStyle>
     )
     
--- src/components/global/Cart/CartContainer.jsx
+++ src/components/global/Cart/CartContainer.jsx
@@ -4,7 +4,10 @@ import {Store} from '../../../store/CartContext';
 import {useContext, useEffect} from 'react';
 
 const CartContainer = () => {
+
     const [data, setData] = useContext(Store);
+
+    //funcion calcular total de la venta
     const totalVenta = () => {
         let resultado = 0;
         let subTotal;
@@ -14,20 +17,48 @@ const CartContainer = () => {
         })
         return resultado;
     }
+    
+    // funcion eliminar item cart, pasando id del producto y id de la venta
+    const removeItem = async (id, idVenta) => {
+        //creo nuevo array sin el producto eliminado
+        const arrayEditado = data.items.filter((item) => item.idProducto !== id )
+        //primero agrego el nuevo array de productos al estado y descuento la cantidad de productos
+        //que fueron agregados a la cantidad general
+        await setData({
+            ...data,
+            items: arrayEditado,
+            cantidad: data.cantidad - data.cantidadItem[idVenta],
+        })
+        //luego elimino la cantidad de cada producto de su array, con el id del mismo
+        await setData({
+            ...data,
+            cantidadItem: data.cantidadItem.splice(idVenta, 1)
+        })
+    }
+    // cada vez que cambie un item se va a ir modificando el precio total
     useEffect(() => {
         let num = totalVenta();
         setData({
             ...data,
             totalVenta: num
         })
-    }, [])
+    }, [data.items])
+
     return (
         <CartContStyle>
             <h2 className="count-carrito">Tu carrito ({data.cantidad})</h2>
+
             {
                 !!data.items &&
-                data.items.map((item, index) => <Cart item={item} cantidadItem={data.cantidadItem[index]} idVenta={index} />)
+                data.items.map((item, index) => <Cart 
+                                                    item={item} 
+                                                    cantidadItem={data.cantidadItem[index]}
+                                                    idVenta={index} 
+                                                    removeItem={removeItem}
+                                                />
+                )
             }
+
             <FootCart>
                 <h2>Total</h2>
                 <span>
@@ -36,10 +67,12 @@ const CartContainer = () => {
                     }
                 </span>
             </FootCart>
+
             <FootCart>
                 <BtnFinalizar className="btn-mas">Elegir mas productos</BtnFinalizar>
                 <BtnFinalizar>Finalizar compra</BtnFinalizar>
             </FootCart>
+
         </CartContStyle>
     )
 }
