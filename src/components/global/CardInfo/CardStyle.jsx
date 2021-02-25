8f71e90ea6fa144d1817916966fb19ded2cfac85
GastonLell <gastonglell@gmail.com>
2021-02-25 02:17:55 -0300

    responsive card-info

--- src/components/global/CardInfo/CardStyle.jsx
+++ src/components/global/CardInfo/CardStyle.jsx
@@ -3,18 +3,23 @@ import { Colores } from "../../utils/Constants";
 
 export const CardInfoStyle = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+  flex-direction: column;
   padding: 50px;
   background-color: ${Colores.colorCinco};
-  max-height: 400px;
+  height: auto;
+
+  @media only screen and (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-around;
+  }
 `;
 export const CardStyle = styled.div`
   border: 1px solid #ccc;
   border-radius: 8px;
   box-shadow: 1px 2px 3px 0.3px;
   padding: 15px 30px;
-  width: 25%;
+  margin-bottom: 20px;
+  width: 100%;
   background-color: #fff;
 
   img {
@@ -31,5 +36,9 @@ export const CardStyle = styled.div`
   h3 {
     font-size: 10px;
     color: ${Colores.colorTres};
+  }
+
+  @media only screen and (min-width: 768px) {
+    width: 25%;
   }
 `;
